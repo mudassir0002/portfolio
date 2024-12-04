354913979b78ac25633abade4a6e3f1f84c04eac
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-12-04 13:03:06 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['media2.dev.to'], // Add the domain here
+  eslint :{
+    ignoreDuringBuilds:true,
   },
 };
 
