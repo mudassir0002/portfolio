0988312b9b6eef9bfe1bb0d2a719ace6230281fe
mudassir0002 <mudassirsiddiqui03@gmail.com>
2026-01-20 20:02:21 +0530

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,19 @@
-// import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import ScrollToTop from "./components/helper/scroll-to-top";
+import ToastProvider from "@/components/ToastProvider";
+
 import "./css/card.scss";
 import "./css/globals.scss";
-import ScrollToTop from "./components/helper/scroll-to-top";
-const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Portfolio of Mudassir Ahmed - Software Developer",
-  description:
-    "This is the portfolio of Mudassir Ahmed. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-};
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <ToastProvider />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
@@ -27,7 +21,6 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
     </html>
   );
 }
