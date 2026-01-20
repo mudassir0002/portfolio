2148147f30894626873f284323fdf1bfcbd88a20
mudassir0002 <mudassirsiddiqui03@gmail.com>
2026-01-20 19:53:12 +0530

    contact page changed

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -4,7 +4,7 @@ import { isValidEmail } from "@/utils/check-email";
 // import axios from "axios";
 import { useState } from "react";
 import { TbMailForward } from "react-icons/tb";
-import { toast } from "react-toastify";
+
 
 function ContactForm() {
   const [error, setError] = useState({ email: false, required: false });
@@ -21,37 +21,36 @@ function ContactForm() {
     }
   };
 
-  const handleSendMail = async (e) => {
-    e.preventDefault();
+const handleSendMail = async (e) => {
+  e.preventDefault();
 
-    if (!userInput.email || !userInput.message || !userInput.name) {
-      setError({ ...error, required: true });
-      return;
-    } else if (error.email) {
-      return;
-    } else {
-      setError({ ...error, required: false });
-    };
+  if (!userInput.email || !userInput.message || !userInput.name) {
+    setError({ ...error, required: true });
+    return;
+  }
 
-    try {
-      setIsLoading(true);
-      // const res = await axios.post(
-      //   `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
-      //   userInput
-      // );
+  if (error.email) return;
 
-      toast.success("Message sent successfully!");
-      setUserInput({
-        name: "",
-        email: "",
-        message: "",
-      });
-    } catch (error) {
-      toast.error(error?.response?.data?.message);
-    } finally {
-      setIsLoading(false);
-    };
-  };
+  try {
+    setIsLoading(true);
+
+    const { toast } = await import("react-toastify");
+
+    toast.success("Message sent successfully!");
+
+    setUserInput({
+      name: "",
+      email: "",
+      message: "",
+    });
+  } catch (err) {
+    const { toast } = await import("react-toastify");
+    toast.error("Something went wrong!");
+  } finally {
+    setIsLoading(false);
+  }
+};
+
 
   return (
     <div>
--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -10,7 +8,23 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-export default function Home() {
+// async function getData() {
+//   const res = await fetch(`https://dev.to/api/articles?username=mudassir0002`)
+
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+
+//   const data = await res.json();
+
+//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+
+//   return filtered;
+// };
+
+export default async function Home() {
+  // const blogs = await getData();
+
   return (
     <>
       <HeroSection />
@@ -19,7 +33,8 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
+      {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </>
-  );
-}
+  )
+};
